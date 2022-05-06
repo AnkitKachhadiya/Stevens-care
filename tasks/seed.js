f7307a72db7b6ea77b28144395bb27390fb734f1
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2022-05-06 13:27:21 -0400

    seed file changes

--- tasks/seed.js
+++ tasks/seed.js
@@ -3,12 +3,12 @@ const data = require("../data/");
 const admin = data.admin;
 
 async function init() {
-    const db = await dbConnection.connectToDb();
+    const db = await dbConnection.connectDb();
     db.dropDatabase();
 
     const adminData = await admin.create("admin@stevens.edu", "stevens@123");
 
-    await dbConnection.closeConnection();
+    await dbConnection.disconnectDb();
 }
 
 init().catch((error) => {
